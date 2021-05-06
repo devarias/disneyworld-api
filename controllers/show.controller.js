871aeb1d92f82121a7a4518b96d80a26e990507f
devarias <gedafu2005@gmail.com>
2021-05-06 16:22:55 -0500

    Added the controller of the shows, to create, to update, to modify, to delete shows

--- controllers/show.controller.js
+++ controllers/show.controller.js
@@ -1,4 +1,3 @@
-const characters = require('../models/characters');
 const shows = require('../models/shows');
 const jwt = require('jsonwebtoken');
 
@@ -7,7 +6,39 @@ async function showList(req, res) {
     if (err) {
       res.sendStatus(403);
     } else {
-      /** Pending */
+      const query = await req.query;
+      if (query.title || query.genre || query.release) {
+        let showsList;
+        if (query.title !== undefined) {
+          showsList = await shows.findAll({
+            where: { title: query.title },
+          });
+        } else if (query.genre !== undefined) {
+          showsList = await shows.findAll({
+            where: {
+              genre: query.genre,
+            },
+          });
+        } else if (query.release !== undefined) {
+          showsList = await characters.findAll({
+            order: ['release', query.release],
+          });
+        }
+        if (showsList === null) {
+          res.status(404).json({ message: 'No Shows found with that filter' });
+          return;
+        } else if (showsList !== null) {
+          res.status(200).json(showsList);
+        }
+      } else {
+        const result = await shows.findAll();
+        const showResult = result.map(({ image, title, release }) => ({
+          image,
+          title,
+          release,
+        }));
+        res.status(200).json(showResult);
+      }
     }
   });
 }
@@ -17,23 +48,48 @@ async function getOneShow(req, res) {
       res.sendStatus(403);
     } else {
       try {
-        /** Pending */
+        const data = await req.params;
+        const show = await shows.findOne({
+          where: { id: +data.id },
+        });
+        if (show === null) {
+          res.status(404).json({ message: 'Show does not found' });
+        } else {
+          res.status(200).json(show);
+        }
       } catch (error) {
-        console.error(error);
+        throw new Error(err);
       }
     }
   });
 }
-
 async function createShow(req, res) {
   jwt.verify(req.token, 'secretToken', async (err) => {
     if (err) {
       res.sendStatus(403);
     } else {
       try {
-        /** Pending */
+        const data = await req.body;
+        const char = await shows.findOne({
+          where: { title: data.title },
+        });
+        if (char !== null) {
+          res.status(409).json({ message: 'Show already exists' });
+        } else {
+          const newShow = await shows.create({
+            title: data.title,
+            characters: data.characters,
+            image: data.image,
+            release: data.release,
+            score: data.score,
+            genre: data.genre,
+          });
+          res
+            .status(201)
+            .json({ message: 'Show created successfully', data: newShow });
+        }
       } catch (error) {
-        console.error(error);
+        throw new Error(err);
       }
     }
   });
@@ -44,9 +100,32 @@ async function editShow(req, res) {
       res.sendStatus(403);
     } else {
       try {
-        /** Pending */
+        const id_ = await req.params.id;
+        const data = await req.body;
+        const char = await shows.findOne({
+          where: { id: id_ },
+        });
+        if (char === null) {
+          res.status(404).json({ message: `Show #${id_} does not found` });
+          return;
+        }
+        const showUpdated = await shows.update(
+          {
+            title: data.title ? data.title : null,
+            image: data.image ? data.image : null,
+            characters: data.characters ? data.characters : null,
+            release: data.release ? data.release : null,
+            score: data.score ? data.score : null,
+            genre: data.genre ? data.genre : null,
+          },
+          { where: { id: id_ } }
+        );
+        res.status(200).json({
+          message: `Show #${id_} was updated`,
+          data: showUpdated,
+        });
       } catch (error) {
-        console.error(error);
+        throw new Error(err);
       }
     }
   });
@@ -57,9 +136,20 @@ async function deleteShow(req, res) {
       res.sendStatus(403);
     } else {
       try {
-        /** Pending */
+        const data = await req.params;
+        const show = await shows.findOne({
+          where: { id: data.id },
+        });
+        if (show === null) {
+          res.status(404).json({ message: `Show #${data.id} does not found` });
+        } else {
+          await characters.destroy({
+            where: { id: data.id },
+          });
+          res.status(200).json({ message: `Show ${data.id} was deleted` });
+        }
       } catch (error) {
-        console.error(error);
+        throw new Error(err);
       }
     }
   });
